3b0dbacb7c0d171ef97b6d7cc43985b3912d3f01
iamzubairdev <muhammad.zubair.i.am@gmail.com>
2025-04-13 20:25:13 +0500

    client - route constants

---
+++ client/src/constants/routes.js
@@ -0,0 +1,7 @@
+export const RouteConstants = {
+    NOT_FOUND: '*',
+    ROOT: '',
+    SIGN_IN: '/signin',
+    SIGN_UP: '/signup',
+    POSTS: '/posts',
+}
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,29 +2,30 @@ import { Navigate } from "react-router-dom";
 import SignIn from "../pages/Auth/SignIn";
 import SignUp from "../pages/Auth/SignUp";
 import Home from "../pages/Home";
+import { RouteConstants } from "../constants";
 
 export const publicRoutes = [
     {
-      path: '/signin',
+      path: RouteConstants.SIGN_IN,
       element: <SignIn />,
     },
     {
-      path: '/signup',
+      path: RouteConstants.SIGN_UP,
       element: <SignUp />,
     },
     { 
-      path: '*',
+      path: RouteConstants.NOT_FOUND,
       element: <Navigate to='/signin' />,
     },
   ];
   
   export const privateRoutes = [
     {
-      path: '',
+      path: RouteConstants.ROOT,
       element: <Navigate to='/posts' />,
     },
     {
-      path: '/posts',
+      path: RouteConstants.POSTS,
       element: <Home />,
     },
   ]
