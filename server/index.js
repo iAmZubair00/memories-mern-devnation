6fd0aa66c4ea1e98a158e155918b0056a3f4b411
ZubairDevDev <m.zubair@adalfi.com>
2025-04-18 22:00:04 +0500

    logger used

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import app from "./server.js";
 import config from "./config/config.js";
+import logger from "./config/logger.js";
 
 mongoose
   .connect(config.dbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(config.port, () => console.log(`server running at PORT ${config.port}`))
+    app.listen(config.port, () => logger.info(`server running at PORT ${config.port}`))
   )
-  .catch((err) => console.log(`${err} server did not connect`));
+  .catch((err) => logger.error(`${err} server did not connect`));
